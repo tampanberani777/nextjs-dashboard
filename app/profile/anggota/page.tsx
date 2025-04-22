ee372db3ff8dd2f0b4762eb03da0f24924adc047
tampanberani777 <dannnsuryo@gmail.com>
2025-04-22 23:52:54 +0700

    update.tsx

--- app/profile/anggota/page.tsx
+++ app/profile/anggota/page.tsx
@@ -49,7 +49,6 @@ export default function ProfilePage() {
         </div>
       </div>
 
-      {/* Tombol ke profil toko */}
     
       <Link href="/profile/toko">
         <button className="fixed bottom-4 right-4 bg-blue-600 text-white px-4 py-2 rounded-full shadow-lg hover:bg-blue-500">
