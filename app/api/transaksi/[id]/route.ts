34e88d0aa543ee683db2a29e5e744e8bdd771674
tampanberani777 <dannnsuryo@gmail.com>
2025-05-31 18:15:49 +0700

    a

--- app/api/transaksi/[id]/route.ts
+++ app/api/transaksi/[id]/route.ts
@@ -1,5 +1,5 @@
 import { NextRequest } from 'next/server';
-import { sql } from '../../../../lib/neondb';
+import { sql } from '../../../lib/neondb';
 
 export async function PUT(request: NextRequest, { params }: { params: { id: string } }) {
   const body = await request.json();
